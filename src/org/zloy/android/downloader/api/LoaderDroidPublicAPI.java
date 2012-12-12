1fc17cd9f94a874381e250a747e01eca14d75739
Dmitry Voronkevich <dmitry.voronkevich@gmail.com>
2012-12-12 20:00:21 +0200

    Added support for add loading silently and to get response from LD

--- src/org/zloy/android/downloader/api/LoaderDroidPublicAPI.java
+++ src/org/zloy/android/downloader/api/LoaderDroidPublicAPI.java
@@ -13,6 +13,7 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.text.TextUtils;
 
 /**
  * Public API for Loader Droid download manager.
@@ -34,10 +35,36 @@ public class LoaderDroidPublicAPI {
 	public static final Uri LOADER_DROID_MARKET_URI = Uri.parse("https://play.google.com/store/apps/details?id="+LOADER_DROID_PACKAGE);
 	public static final String ALTERNATIVE_DOWNLOAD_LINK = "http://loader-droid-public-api.googlecode.com/files/LoaderDroid-0.8.1.apk";
 	
+	public static final String RESULT_ACTION_DOWNLOAD_COMPLETED = "org.zloy.android.downloader.action.DOWNLOAD_COMPLETED";
+	public static final String RESULT_ACTION_DOWNLOAD_FAILED = "org.zloy.android.downloader.action.DOWNLOAD_FAILED";
+	public static final String RESULT_EXTRA_FILE = "file";
+	
 	// First version which supports open api
 	private static final int MINIMUM_VERSION = 44;
+	private static final int VERSION_WITH_SILENT_ADD = 45;
 	
 	public static int API_VERSION = 1; 
+	
+	
+	/**
+	 * Checks installed LoaderDroid on system and returns its version. Or -1 if no loader droid installed on system.
+	 * 
+	 * @param ctx
+	 * @return -1 or version of LoaderDroid
+	 */
+	public static int getLoaderDroidVersion(Context ctx) {
+		PackageManager packageManager = ctx.getPackageManager();
+		if (packageManager == null) // sometimes this can happen on strange devices
+			return -1;
+		List<PackageInfo> packages = packageManager.getInstalledPackages(0);
+		if (packages == null)
+			return -1;
+		for (PackageInfo info: packages) {
+			if (info != null && LOADER_DROID_PACKAGE.equals(info.packageName))
+				return info.versionCode;
+		}
+		return -1;
+	}
 	
 	/**
 	 * Checks whether LoaderDroid is installed on device
@@ -46,17 +73,7 @@ public class LoaderDroidPublicAPI {
 	 * @return
 	 */
 	public static boolean isLoaderDroidInstalled(Context ctx) {
-		PackageManager packageManager = ctx.getPackageManager();
-		if (packageManager == null) // sometimes this can happen on strange devices
-			return false;
-		List<PackageInfo> packages = packageManager.getInstalledPackages(0);
-		if (packages == null)
-			return false;
-		for (PackageInfo info: packages) {
-			if (info != null && LOADER_DROID_PACKAGE.equals(info.packageName) && info.versionCode >= MINIMUM_VERSION)
-				return true;
-		}
-		return false;
+		return getLoaderDroidVersion(ctx) >= MINIMUM_VERSION;
 	}
 	
 	/**
@@ -66,17 +83,7 @@ public class LoaderDroidPublicAPI {
 	 * @return true if LoaderDroid need update
 	 */
 	public static boolean isLoaderDroidRequireUpdate(Context ctx) {
-		PackageManager packageManager = ctx.getPackageManager();
-		if (packageManager == null) // sometimes this can happen on strange devices
-			return false;
-		List<PackageInfo> packages = packageManager.getInstalledPackages(0);
-		if (packages == null)
-			return false;
-		for (PackageInfo info: packages) {
-			if (info != null && LOADER_DROID_PACKAGE.equals(info.packageName) && info.versionCode < MINIMUM_VERSION)
-				return true;
-		}
-		return false;
+		return getLoaderDroidVersion(ctx) < MINIMUM_VERSION;
 	}
 	
 	/**
@@ -143,13 +150,22 @@ public class LoaderDroidPublicAPI {
 	 * @return false if LD is not installed on device
 	 */
 	public static boolean addLoading(MissingLDAction missingLDAction, Context ctx, String url, String name, AllowedConnection allowedConnection, Directory directory) {
+		final int loaderVersion = getLoaderDroidVersion(ctx);
+		
 		Intent intent;
-		if (missingLDAction == MissingLDAction.ASK_USER_TO_INSTALL_LD)
-			intent = new Intent(ctx, AddLoadingActivity.class);
-		else {
-			if (!isLoaderDroidInstalled(ctx))
+		boolean allowSilentAdd = false;
+		// No loader droid, or it should be updated
+		if (loaderVersion < MINIMUM_VERSION) {
+			if (missingLDAction == MissingLDAction.ASK_USER_TO_INSTALL_LD) {
+				intent = new Intent(ctx, AddLoadingActivity.class);
+			} else {
 				return false;
+			}
+		} else {
 			intent = new Intent(ACTION_ADD_LOADING);
+			if (loaderVersion >= VERSION_WITH_SILENT_ADD) {
+				allowSilentAdd = isSilentMode(name, allowedConnection, directory);
+			}
 		}
 		
 		intent.setData(Uri.parse(url));
@@ -166,11 +182,89 @@ public class LoaderDroidPublicAPI {
 		}
 		intent.putExtra(EXTRA_API_VERSION, API_VERSION); // For future releases
 		try {
-			ctx.startActivity(intent);
+			if (allowSilentAdd)
+				ctx.sendBroadcast(intent);
+			else
+				ctx.startActivity(intent);
 			return true;
 		} catch (ActivityNotFoundException e) {
 			return false;
 		}
+	}
+	
+	
+	/**
+	 * Every time LoaderDroid completes loading, it sends broadcast message 
+	 * with org.zloy.android.downloader.action.DOWNLOAD_COMPLETED.
+	 * 
+	 * Intent will contain initial url as an intent data and absolute file path as string extra "file".
+	 * 
+	 * Use {@link #getInitialUrl(Intent)} and {@link #getDownloadedFilePath(Intent)} to extract them.
+	 *  
+	 * @param intent
+	 * @return
+	 */
+	public static boolean isSuccessfullDownload(Intent intent) {
+		if (intent == null)
+			return false;
+		return RESULT_ACTION_DOWNLOAD_COMPLETED.equals(intent.getAction());
+	}
+
+	/**
+	 * Every time LoaderDroid fails loading, it sends broadcast message 
+	 * with org.zloy.android.downloader.action.DOWNLOAD_FAILED.
+	 * 
+	 * Intent will contain initial url as an intent data.
+	 * 
+	 * Use {@link #getInitialUrl(Intent)} to extract it.
+	 *  
+	 * @param intent
+	 * @return
+	 */
+	public static boolean isFailedDownload(Intent intent) {
+		if (intent == null)
+			return false;
+		return RESULT_ACTION_DOWNLOAD_FAILED.equals(intent.getAction());
+	}
+	
+	/**
+	 * Extracts absolute file path from download_completed itent from LoaderDroid
+	 * 
+	 * @param intent
+	 * @return
+	 * 
+	 * @see #isSuccessfullDownload(Intent)
+	 */
+	public static String getDownloadedFilePath(Intent intent) {
+		if (intent == null)
+			return null;
+		return intent.getStringExtra(RESULT_EXTRA_FILE);
+	}
+	
+	/**
+	 * Returns initial url which was downloaded of failed from broadcast message from LD
+	 * 
+	 * @param intent
+	 * @return
+	 * 
+	 * @see #isFailedDownload(Intent)
+	 * @see #isSuccessfullDownload(Intent)
+	 */
+	public static String getInitialUrl(Intent intent) {
+		if (intent == null)
+			return null;
+		return intent.getDataString();
+	}
+	
+	private static boolean isSilentMode(String name,
+			AllowedConnection allowedConnection, Directory directory) {
+		if (TextUtils.isEmpty(name))
+			return false;
+		if (directory == null || directory == Directory.ASK_USER_FOR_DIRECTORY)
+			return false;
+		if (allowedConnection == null || allowedConnection == AllowedConnection.ASK_USER)
+			return false;
+		return true;
 	}
 
 }
